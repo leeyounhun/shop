96c6669ea63b3c94ef24f17f5362b15616176ab6
leeyounhun <107522866+leeyounhun@users.noreply.github.com>
2022-12-09 10:48:12 +0900

    feat: Order History

---
+++ src/main/java/com/lyh/shop/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.lyh.shop.controller;
+
+import com.lyh.shop.dto.OrderHistDto;
+import com.lyh.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping({"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page")Optional<Integer> page, Principal principal, Model model) {
+        Pageable pageable = PageRequest.of(page.orElse(0), 6);
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+        return "order/orderHist";
+    }
+
+}
